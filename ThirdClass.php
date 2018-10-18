a099cd0eaa69a4c38f830a021c3a5e0d8fc54440
Mateusz Brzozowski <neertoon@icloud.com>
2018-10-18 12:37:53 +0200

    ------- ZACZYNAMY --------

---
+++ ThirdClass.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: neert
+ * Date: 2018-10-18
+ * Time: 12:37
+ */
+
+class ThirdClass
+{
+    public function first() {
+        $var = 1;
+    }
+
+    public function secound() {
+        $var = 2;
+    }
+
+    public function third() {
+        $var = 3;
+    }
+}
