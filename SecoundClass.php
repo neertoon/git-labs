8cf2012f6947ead34ccc195aa9e0ce4b0110a3c1
Mateusz Brzozowski <neertoon@icloud.com>
2018-10-18 12:33:26 +0200

    NOWY PLIK - testujemy

---
+++ SecoundClass.php
@@ -0,0 +1,16 @@
+<?php
+
+class SecoundClass
+{
+    public function first() {
+        $var = 1;
+    }
+
+    public function secound() {
+        $var = 1;
+    }
+
+    public function third() {
+        $var = 1;
+    }
+}
