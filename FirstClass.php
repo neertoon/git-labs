6831d8ac9e4605fb6ecf6632f565ae140546ee37
Mateusz Brzozowski <neertoon@icloud.com>
2018-10-18 11:46:08 +0200

    First commit

---
+++ FirstClass.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class FirstClass
+{
+    public function first() {
+        //some code 1
+        $var = 1;
+    }
+    
+    public function secound() {
+        //some code 2
+        $var = 2;
+    }
+    
+    public function third() {
+        //sobe code 3
+        $var = 3;
+    }
+}
