656cf96a51d72d5b30e2c8dd7648a9fb241b9920
Mateusz Brzozowski <neertoon@icloud.com>
2018-10-24 13:16:28 +0200

    ------- ZACZYNAMY -------

---
+++ FourClass.php
@@ -0,0 +1,16 @@
+<?php
+
+class FourClass
+{
+    public function first() {
+        $var = 1;
+    }
+
+    public function secound() {
+        $var = 2;
+    }
+
+    public function third() {
+        $var = 3;
+    }
+}
