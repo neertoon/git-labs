8cd352af02b423298b1cff6223f51d21704f3147
Mateusz Brzozowski <neertoon@icloud.com>
2018-12-13 09:50:34 +0100

    --- START ---

--- SecoundClass.php
+++ SecoundClass.php
@@ -4,17 +4,10 @@ class SecoundClass
 {
     public function first() {
         $var = 1;
-        $zmianaWMiedzyCzasieFeature2 = '';
-        $terazZabawaWReverty = '';
-        $zrobmyCommit1 = '';
-        $zrobmyCommit2 = '';
-        $zrobmyCommit3 = '';
-        $commit5 = '';
     }
 
     public function secound() {
         $var = 1;
-        $zmianaPoMerguFeature2 = '';
     }
 
     public function third() {
