6c16c22a83ea9544e0cbb660b35598808d78b7b7
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2023-06-14 08:26:20 +0200

    TEGO POTRZEBUJE Z MASTERA #INNYTASK

--- FirstClass.php
+++ FirstClass.php
@@ -5,7 +5,7 @@ class FirstClass
 {
     public function first() {
         //some code 1
-        $var = 12;
+        $var = 123;
         $inMeanTimeIAddNewCodeToMaster = '';
         
         $masterBugFix = '1';
